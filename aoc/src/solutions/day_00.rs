d422abb0e60910ccc3903923d37d548e51062bc3
Daniel Balchev <dbalchev@gmail.com>
2024-11-23 00:59:59 +0200

    Tie up the literal to the preceeding part

--- aoc/src/solutions/day_00.rs
+++ aoc/src/solutions/day_00.rs
@@ -1,34 +1,35 @@
 use aoc_utils::MyResult;
 
 macro_rules! formatted_struct {
-    (struct $struct_name:ident {$($token:tt)*}) => {
-        formatted_struct!{in_process $struct_name ($($token)*) -> ( )}
-    };
-    (in_process $struct_name:ident ($first_name:ident : $first_type:ty , $($lit:literal, $name:ident : $type:ty),*) -> ($($result:tt)*)) => {
-        formatted_struct!{in_process $struct_name ( )-> ($($result)*  $first_name:$first_type, $($name:$type),*) }
-    };
     (in_process $struct_name:ident ()-> ($($result:tt)*)) => {
         struct $struct_name {
             $($result)*
         }
-    }
+    };
+    (struct $struct_name:ident {$($name:ident : $type:ty , $($lit:literal)?),* }) => {
+        formatted_struct!{in_process $struct_name ($($name : $type , $($lit)?),* ) -> ( )}
+    };
+    (in_process $struct_name:ident ($($name:ident : $type:ty , $($lit:literal)?),* ) -> ($($result:tt)*)) => {
+        formatted_struct!{in_process $struct_name ( )-> ($($result)*  $($name:$type),*  ) }
+    };
+
 }
 
 formatted_struct! {
     struct Foo {
             foo:String,
             "bar",
-            bar:i32
+            bar:i32,
     }
 }
 
 pub type InputFormat = String;
 
 pub fn solve_1(input: &InputFormat) -> MyResult<String> {
-    let f = Foo {
-        foo: "x".to_string(),
-        bar: 3,
-    };
+    // let f = Foo {
+    //     foo: "x".to_string(),
+    //     bar: 3,
+    // };
     Ok(format!("Hello {}", input))
 }
 
