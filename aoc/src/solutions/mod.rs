367a1af31c27ccbcd04a237328b9b97d29340ece
Daniel Balchev <dbalchev@gmail.com>
2024-12-01 10:43:05 +0200

    Refactor mod.rs

--- aoc/src/solutions/mod.rs
+++ aoc/src/solutions/mod.rs
@@ -1,25 +1,16 @@
 #![allow(clippy::vec_init_then_push)]
 use aoc_utils::{make_day_solution, ExistentialDaySolution};
 
-macro_rules! declare_days {
+macro_rules! register_days {
     ($($i:ident),*) => {
         $(mod $i;)*
-    };
-}
-macro_rules! make_day_solution_items {
-    ($v: ident, $($i:ident),*) => {
-        $($v.push(make_day_solution::<$i::Solution>(concat!(stringify!($i), ".rs"))));*
-    };
-}
-macro_rules! register_days {
-    ($($is:ident),*) => {
-        declare_days! {$($is),*}
         pub fn make_day_solutions() -> Vec<ExistentialDaySolution> {
-            let mut result = Vec::new();
-            make_day_solution_items!(result, $($is),*);
-            result
+            vec![
+                $(
+                    make_day_solution::<$i::Solution>(concat!(stringify!($i), ".rs")),
+                )*
+            ]
         }
     };
 }
 register_days! {day_00, day_01}
-// include! {concat!(env!("OUT_DIR"), "/days.fragment")}
