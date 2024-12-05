d9c907cc14c6057662aec1dd55a8877d3c2b2382
Daniel Balchev <dbalchev@gmail.com>
2024-12-05 09:13:46 +0200

    Day 05: Part 2

--- aoc/src/solutions/day_05.rs
+++ aoc/src/solutions/day_05.rs
@@ -3,7 +3,7 @@ use std::{
     fmt::Debug,
 };
 
-use aoc_utils::{formatted_struct, DaySolution, MyResult};
+use aoc_utils::{formatted_struct, DaySolution, MyResult, Parsable};
 
 formatted_struct! {
     #[derive(Debug)]
@@ -15,7 +15,7 @@ formatted_struct! {
 }
 
 formatted_struct! {
-    #[derive(Debug)]
+    #[derive(Debug, Clone)]
     pub struct Update {
         #[separated_by=","]
         page_numbers: Vec<i32>,
@@ -33,39 +33,98 @@ formatted_struct! {
     }
 }
 
-pub struct Solution;
+pub struct ProcessedInput {
+    page_to_pages_after: HashMap<i32, HashSet<i32>>,
+    updates: Vec<Update>,
+}
 
-impl DaySolution for Solution {
-    type InputFormat = InputFormat;
-    fn solve_1(input: &InputFormat) -> MyResult<impl Debug + 'static> {
+impl Parsable for ProcessedInput {
+    fn parse(text: &str) -> MyResult<Self> {
+        let InputFormat {
+            updates,
+            ordering_rules,
+        } = InputFormat::parse(text)?;
         let mut page_to_pages_after = HashMap::new();
+
         for &OrderingRule {
             page_before,
             page_after,
-        } in &input.ordering_rules
+        } in &ordering_rules
         {
             page_to_pages_after
                 .entry(page_before)
                 .or_insert_with(|| HashSet::new())
                 .insert(page_after);
         }
-        let check_updates = |&Update { ref page_numbers }| {
-            let mut past_pages = HashSet::new();
-            for &page in page_numbers {
-                if let Some(pages_after) = page_to_pages_after.get(&page) {
-                    if !past_pages.is_disjoint(pages_after) {
-                        return false;
-                    }
+        Ok(ProcessedInput {
+            page_to_pages_after,
+            updates,
+        })
+    }
+}
+
+impl ProcessedInput {
+    fn check_update(&self, update: &Update) -> bool {
+        let mut past_pages = HashSet::new();
+        for &page in &update.page_numbers {
+            if let Some(pages_after) = self.page_to_pages_after.get(&page) {
+                if !past_pages.is_disjoint(pages_after) {
+                    return false;
                 }
-                past_pages.insert(page);
             }
-            return true;
-        };
+            past_pages.insert(page);
+        }
+        return true;
+    }
+    fn sort_update(&self, update: Update) -> Update {
+        let Update { page_numbers } = update;
+        let mut sorted_page_numbers = Vec::new();
+
+        for current_page in page_numbers {
+            let insert_index = sorted_page_numbers
+                .iter()
+                .enumerate()
+                .filter_map(|(index, past_page)| {
+                    let pages_after = self.page_to_pages_after.get(&current_page)?;
+                    if pages_after.contains(past_page) {
+                        Some(index)
+                    } else {
+                        None
+                    }
+                })
+                .min()
+                .unwrap_or(sorted_page_numbers.len());
+            sorted_page_numbers.insert(insert_index, current_page);
+        }
+        Update {
+            page_numbers: sorted_page_numbers,
+        }
+    }
+}
+
+pub struct Solution;
+
+impl DaySolution for Solution {
+    type InputFormat = ProcessedInput;
+    fn solve_1(input: &ProcessedInput) -> MyResult<impl Debug + 'static> {
         let result = input
             .updates
             .iter()
-            .filter(|u| check_updates(u))
+            .filter(|u| input.check_update(u))
             .map(|&Update { ref page_numbers }| page_numbers[page_numbers.len() / 2])
+            .sum::<i32>();
+
+        Ok(result)
+    }
+    fn solve_2(input: &ProcessedInput) -> MyResult<impl Debug + 'static> {
+        let result = input
+            .updates
+            .iter()
+            .filter(|u| !input.check_update(u))
+            .cloned()
+            .map(|u| input.sort_update(u))
+            .map(|Update { page_numbers }| page_numbers[page_numbers.len() / 2])
+            // .collect::<Vec<_>>();
             .sum::<i32>();
 
         Ok(result)
