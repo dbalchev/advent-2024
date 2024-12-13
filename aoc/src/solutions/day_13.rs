1a398613841d5d64b9f8d7392f76c66db0fd2812
Daniel Balchev <dbalchev@gmail.com>
2024-12-13 09:39:19 +0200

    Day 13: Part 2

--- aoc/src/solutions/day_13.rs
+++ aoc/src/solutions/day_13.rs
@@ -35,8 +35,21 @@ formatted_struct! {
     }
 }
 
+fn invert_discrete(m: [[i64; 2]; 2]) -> ([[i64; 2]; 2], i64) {
+    let [[a, b], [c, d]] = m;
+    let denominator = a * d - b * c;
+    let numerator = [[d, -b], [-c, a]];
+    (numerator, denominator)
+}
+
+fn mul(m: [[i64; 2]; 2], v: [i64; 2]) -> [i64; 2] {
+    let [[a, b], [c, d]] = m;
+    let [x, y] = v;
+    [a * x + b * y, c * x + d * y]
+}
+
 impl ClawMachine {
-    fn solve_1(&self) -> Option<i64> {
+    fn _solve_1(&self) -> Option<i64> {
         let mut memo = make_recursive_fn(
             |rec_memo: &mut dyn FnMut((i64, i64)) -> Option<i64>, (x, y)| {
                 if x == 0 && y == 0 {
@@ -52,6 +65,34 @@ impl ClawMachine {
         );
         memo((self.prize_x, self.prize_y))
     }
+    fn math_solve_1(&self) -> Option<i64> {
+        self.math_solve(self.prize_x, self.prize_y)
+    }
+    fn math_solve_2(&self) -> Option<i64> {
+        const DELTA: i64 = 10000000000000;
+        self.math_solve(DELTA + self.prize_x, DELTA + self.prize_y)
+    }
+    fn math_solve(&self, target_x: i64, target_y: i64) -> Option<i64> {
+        // a_press * a_x + b_press * b_x = x
+        // a_press * a_y + b_press * b_y = y
+        // P (a b) = (x y)
+        // (a b) = P ^^ -1 (x y)
+        // a = a_x
+        // b = b_x
+        let m = [
+            [self.a.x_delta, self.b.x_delta],
+            [self.a.y_delta, self.b.y_delta],
+        ];
+        let (im_n, im_d) = invert_discrete(m);
+        let [a_n, b_n] = mul(im_n, [target_x, target_y]);
+        if im_d == 0 {
+            todo!("not yet handling 0 det")
+        }
+        if a_n % im_d != 0 || b_n % im_d != 0 {
+            return None;
+        }
+        return Some(a_n / im_d * 3 + b_n / im_d);
+    }
 }
 
 pub struct Solution;
@@ -62,8 +103,18 @@ impl DaySolution for Solution {
         let results = input
             .claw_machines
             .iter()
-            .map(ClawMachine::solve_1)
+            .map(ClawMachine::math_solve_1)
             .collect::<Vec<_>>();
+        // Ok(results)
+        Ok(results.into_iter().filter_map(|x| x).sum::<i64>())
+    }
+    fn solve_2(input: &InputFormat) -> MyResult<impl Debug + 'static> {
+        let results = input
+            .claw_machines
+            .iter()
+            .map(ClawMachine::math_solve_2)
+            .collect::<Vec<_>>();
+        // Ok(results)
         Ok(results.into_iter().filter_map(|x| x).sum::<i64>())
     }
 }
