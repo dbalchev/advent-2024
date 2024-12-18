08cb0d6b8befbcab13bb3d8b33c2833b8cc11d38
Daniel Balchev <dbalchev@gmail.com>
2024-12-18 10:27:09 +0200

    Day 18: Part 2

--- aoc/src/solutions/day_18.rs
+++ aoc/src/solutions/day_18.rs
@@ -32,13 +32,17 @@ struct Graph {
 
 const DELTAS: [(i32, i32); 4] = [(1, 0), (0, 1), (-1, 0), (0, -1)];
 
+fn size_n_corruptions(input: &InputFormat) -> ((i32, i32), usize) {
+    match input.locations.len() {
+        25 => ((7, 7), 12),
+        3450 => ((71, 71), 1024),
+        x => panic!("unknown n_locations = {}", x),
+    }
+}
+
 impl Graph {
-    fn new(input: &InputFormat) -> Graph {
-        let (size, n_corruptions) = match input.locations.len() {
-            25 => ((7, 7), 12),
-            3450 => ((71, 71), 1024),
-            x => panic!("unknown n_locations = {}", x),
-        };
+    fn new(input: &InputFormat, n_corruptions: usize) -> Graph {
+        let (size, _) = size_n_corruptions(input);
         let corruptions = input.locations[..n_corruptions]
             .iter()
             .map(|MemoryLocation { x, y }| (*x, *y))
@@ -57,6 +61,21 @@ impl Graph {
             result.push((i, j));
         }
         result
+    }
+    fn go_to_target(&self) -> Option<i32> {
+        let mut state = AStarState::new(self.size);
+        let target = (self.size.0 - 1, self.size.1 - 1);
+        state.push((0, 0), 0);
+
+        while let Some((location, n_steps)) = state.pop() {
+            for adj_location in self.adj_locations(location) {
+                if adj_location == target {
+                    return Some(n_steps + 1);
+                }
+                state.push(adj_location, n_steps + 1);
+            }
+        }
+        None
     }
 }
 
@@ -87,30 +106,40 @@ impl AStarState {
         self.state_queue.push((-remaining, -n_steps, location));
         self.min_steps_per_state.insert(location, n_steps);
     }
-    fn pop(&mut self) -> ((i32, i32), i32) {
-        let (_, negative_n_steps, location) = self.state_queue.pop().unwrap();
-        (location, -negative_n_steps)
+    fn pop(&mut self) -> Option<((i32, i32), i32)> {
+        while let Some((_, negative_n_steps, location)) = self.state_queue.pop() {
+            let n_steps = -negative_n_steps;
+            let min_steps = self.min_steps_per_state.get(&location).unwrap();
+            if n_steps > *min_steps {
+                continue;
+            }
+            return Some((location, n_steps));
+        }
+        None
     }
 }
 
 impl DaySolution for Solution {
     type InputFormat = InputFormat;
     fn solve_1(input: &InputFormat) -> MyResult<impl Debug + 'static> {
-        let graph = Graph::new(input);
-        let mut state = AStarState::new(graph.size);
-        let target = (graph.size.0 - 1, graph.size.1 - 1);
-        state.push((0, 0), 0);
-
-        loop {
-            let (location, n_steps) = state.pop();
-            for adj_location in graph.adj_locations(location) {
-                if adj_location == target {
-                    return Ok(n_steps + 1);
-                }
-                state.push(adj_location, n_steps + 1);
+        let (_, n_corruptions) = size_n_corruptions(input);
+        let graph = Graph::new(input, n_corruptions);
+        Ok(graph.go_to_target().unwrap())
+    }
+    fn solve_2(input: &InputFormat) -> MyResult<impl Debug + 'static> {
+        let mut max_solvable = 0;
+        let mut min_unsolvable = input.locations.len() - 1;
+        while max_solvable + 1 < min_unsolvable {
+            let mid = (max_solvable + min_unsolvable) / 2;
+            assert_ne!(mid, max_solvable);
+            assert_ne!(mid, min_unsolvable);
+            if Graph::new(input, mid + 1).go_to_target().is_some() {
+                max_solvable = mid;
+            } else {
+                min_unsolvable = mid;
             }
         }
-
-        panic!("foo")
+        let breaking_byte = &input.locations[min_unsolvable];
+        Ok((breaking_byte.x, breaking_byte.y))
     }
 }
