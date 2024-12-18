0217486b40e700a3a026fe5e89207b163106e759
Daniel Balchev <dbalchev@gmail.com>
2024-12-18 23:22:22 +0200

    Day 10: Part 2

--- aoc/src/solutions/day_10.rs
+++ aoc/src/solutions/day_10.rs
@@ -57,6 +57,50 @@ impl DaySolution for Solution {
             .map(|r| r.iter().map(HashSet::len).sum::<usize>())
             .sum::<usize>())
     }
+
+    fn solve_2(input: &InputFormat) -> MyResult<impl Debug + 'static> {
+        let mut reachable_ends = input
+            .rows
+            .iter()
+            .map(|Chars(row)| row.iter().map(|_| 0).collect::<Vec<_>>())
+            .collect::<Vec<_>>();
+        for (i, row) in reachable_ends.iter_mut().enumerate() {
+            for (j, c) in row.iter_mut().enumerate() {
+                if input.rows[i].0[j] == '9' {
+                    *c = 1;
+                }
+            }
+        }
+        for current in ('0'..='8').rev() {
+            let mut new_reachable_ends = input
+                .rows
+                .iter()
+                .map(|Chars(row)| row.iter().map(|_| 0).collect::<Vec<_>>())
+                .collect::<Vec<_>>();
+            for (i, row) in new_reachable_ends.iter_mut().enumerate() {
+                for (j, c) in row.iter_mut().enumerate() {
+                    if input.rows[i].0[j] == current {
+                        for (di, dj) in [(0, 1), (0, -1), (-1, 0), (1, 0)] {
+                            let ci = i as i32 + di;
+                            let cj = j as i32 + dj;
+                            let in_bounds = (0..(reachable_ends.len() as i32)).contains(&ci)
+                                && (0..(reachable_ends[ci as usize].len() as i32)).contains(&cj);
+                            if in_bounds {
+                                *c += reachable_ends[ci as usize][cj as usize];
+                            }
+                        }
+                    }
+                }
+            }
+            reachable_ends = new_reachable_ends;
+        }
+
+        // println!("{:?}", reachable_ends);
+        Ok(reachable_ends
+            .iter()
+            .map(|r| r.iter().sum::<usize>())
+            .sum::<usize>())
+    }
     fn preferred_sample_input() -> i32 {
         4
     }
