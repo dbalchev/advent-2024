392603c665934922a7fd9aea1258efcf575db653
Daniel Balchev <dbalchev@gmail.com>
2024-12-18 23:18:55 +0200

    Day 10: Part 1

---
+++ aoc/src/solutions/day_10.rs
@@ -0,0 +1,63 @@
+use std::{collections::HashSet, fmt::Debug};
+
+use aoc_utils::{formatted_struct, Chars, DaySolution, MyResult};
+
+formatted_struct! {
+    #[derive(Debug)]
+    pub struct InputFormat {
+        #[separated_by="\n"]
+        rows: Vec<Chars>,
+    }
+}
+
+pub struct Solution;
+
+impl DaySolution for Solution {
+    type InputFormat = InputFormat;
+    fn solve_1(input: &InputFormat) -> MyResult<impl Debug + 'static> {
+        let mut reachable_ends = input
+            .rows
+            .iter()
+            .map(|Chars(row)| row.iter().map(|_| HashSet::new()).collect::<Vec<_>>())
+            .collect::<Vec<_>>();
+        for (i, row) in reachable_ends.iter_mut().enumerate() {
+            for (j, c) in row.iter_mut().enumerate() {
+                if input.rows[i].0[j] == '9' {
+                    c.insert((i, j));
+                }
+            }
+        }
+        for current in ('0'..'9').rev() {
+            let mut new_reachable_ends = input
+                .rows
+                .iter()
+                .map(|Chars(row)| row.iter().map(|_| HashSet::new()).collect::<Vec<_>>())
+                .collect::<Vec<_>>();
+            for (i, row) in new_reachable_ends.iter_mut().enumerate() {
+                for (j, c) in row.iter_mut().enumerate() {
+                    if input.rows[i].0[j] == current {
+                        for (di, dj) in [(0, 1), (0, -1), (-1, 0), (1, 0)] {
+                            let ci = (i as i32 + di);
+                            let cj = (j as i32 + dj);
+                            let in_bounds = (0..(reachable_ends.len() as i32)).contains(&ci)
+                                && (0..(reachable_ends[ci as usize].len() as i32)).contains(&cj);
+                            if in_bounds {
+                                c.extend(reachable_ends[ci as usize][cj as usize].iter());
+                            }
+                        }
+                    }
+                }
+            }
+            reachable_ends = new_reachable_ends;
+        }
+
+        // println!("{:?}", reachable_ends);
+        Ok(reachable_ends
+            .iter()
+            .map(|r| r.iter().map(HashSet::len).sum::<usize>())
+            .sum::<usize>())
+    }
+    fn preferred_sample_input() -> i32 {
+        4
+    }
+}
