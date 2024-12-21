b657a9991a07a6485672cf4e0bcebf5b09547941
Daniel Balchev <dbalchev@gmail.com>
2024-12-21 18:53:11 +0200

    Day 12: Part 2

--- aoc/src/solutions/day_12.rs
+++ aoc/src/solutions/day_12.rs
@@ -83,6 +83,40 @@ fn label_score(labels: &[Vec<i32>], target_label: i32) -> i32 {
     area * perimeter
 }
 
+fn label_sides_score(labels: &[Vec<i32>], target_label: i32) -> (i32, i32) {
+    let mi = labels.len() as isize;
+    let mj = labels[0].len() as isize;
+    let mut area = 0;
+
+    let flex_label = |i, j| {
+        if (0..mi as isize).contains(&i) && (0..mj as isize).contains(&j) {
+            labels[i as usize][j as usize]
+        } else {
+            -1
+        }
+    };
+    for i in 0..mi {
+        for j in 0..mj {
+            area += (flex_label(i, j) == target_label) as i32;
+        }
+    }
+
+    let mut n_sides = 0;
+
+    for i in -1..mi {
+        for j in -1..mj {
+            let a = (flex_label(i, j) == target_label) as i32;
+            let b = (flex_label(i + 1, j) == target_label) as i32;
+            let c = (flex_label(i, j + 1) == target_label) as i32;
+            let d = (flex_label(i + 1, j + 1) == target_label) as i32;
+            let n_insides = a + b + c + d;
+            n_sides += (n_insides % 2 == 1) as i32 + 2 * (a == d && c == b && a != b) as i32;
+        }
+    }
+
+    (area, n_sides)
+}
+
 impl DaySolution for Solution {
     type InputFormat = InputFormat;
     fn solve_1(input: &InputFormat) -> MyResult<impl Debug + 'static> {
@@ -90,5 +124,17 @@ impl DaySolution for Solution {
         let result = (0..n_labels).map(|i| label_score(&regions, i)).sum::<i32>();
         // .collect::<Vec<_>>();
         Ok(result)
+    }
+    fn solve_2(input: &InputFormat) -> MyResult<impl Debug + 'static> {
+        let (regions, n_labels) = label_regions(&input.rows);
+        let result = (0..n_labels)
+            .map(|i| label_sides_score(&regions, i))
+            .map(|(a, b)| a * b)
+            .sum::<i32>();
+        // .collect::<Vec<_>>();
+        Ok(result)
+    }
+    fn preferred_sample_input() -> i32 {
+        6
     }
 }
